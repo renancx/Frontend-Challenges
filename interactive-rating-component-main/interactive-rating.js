66719ab51d6ebf6d4a950560d89690b30273c148
renancx <renan.lowe@hotmail.com>
2024-10-17 16:27:29 -0300

    JS code finished

--- interactive-rating-component-main/interactive-rating.js
+++ interactive-rating-component-main/interactive-rating.js
@@ -1,13 +1,21 @@
 function selectRatingNumber(numberId) {
     clearSelection();
-
-    var selectedNumber = document.getElementById(numberId);
-
-    selectedNumber.classList.add('selected');
+    document.getElementById(numberId).classList.add('selected');
 }
 
 function clearSelection() {
-    document.querySelectorAll('.rating-select').forEach(e => {
-        e.classList.remove('selected');
-    });
+    document.querySelectorAll('.rating-select').forEach(e => e.classList.remove('selected'));
+}
+
+function submitValue() {
+    const selectedElement = document.querySelector('.selected');
+
+    if (selectedElement) {
+        const spanValue = selectedElement.querySelector('span').textContent;
+        document.getElementById('rating-element').style.display = 'none';
+        document.getElementById('after-element').style.display = 'flex';
+        document.getElementById('selected-element').textContent = spanValue;
+    } else {
+        alert("Nenhum rating foi selecionado.");
+    }
 }
