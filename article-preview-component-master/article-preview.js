e0c7b25693eccda8ff5041faae517af57e633ab3
renancx <renan.lowe@hotmail.com>
2024-10-31 17:11:59 -0300

    pop-up initial design tests

---
+++ article-preview-component-master/article-preview.js
@@ -0,0 +1,7 @@
+function showSocialLinks() {
+    const svg = document.getElementById('share-svg')
+    const path = svg.querySelector('path');
+
+    svg.style.backgroundColor = '#6e7f95';
+    path.setAttribute('fill', '#FFFFFF');
+}
